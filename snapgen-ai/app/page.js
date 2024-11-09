eac39ce130583d7ef0d1c0b667ec714e1f694d78
shraddha <shr.comp1@gmail.com>
2024-11-10 02:44:20 +0530

    connecting neon db with drizzle orm

--- snapgen-ai/app/page.js
+++ snapgen-ai/app/page.js
@@ -1,9 +1,11 @@
+import { Button } from "@/components/ui/button";
 import Image from "next/image";
 
 export default function Home() {
   return (
     <div>
       <h1>Shraddha</h1>
+      <Button variant="default">Click</Button>
     </div>
   );
 }
